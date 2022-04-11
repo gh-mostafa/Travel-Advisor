886da48fc391b86910a4a8b1f713354da8aff77d
gh-mostafa <72677567+gh-mostafa@users.noreply.github.com>
2022-04-11 11:24:11 +0430

    upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+//npm install @material-ui/core @material-ui/icons @material-ui/lab @react-google-maps/api axios google-map-react
